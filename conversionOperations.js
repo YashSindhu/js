0a562716a843b7a905daba05e3e477fa6e82e17d
Yash Sindhu <yashsindhu123@gmail.com>
2025-07-10 15:42:39 +0530

    ConversoinOperations

---
+++ conversionOperations.js
@@ -0,0 +1,24 @@
+let score = null  //"33abc"
+// console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));  
+// console.log(valueInNumber) // => NaN => Not A Number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let LoggedIn = "Yash"
+let boolLoggedIn = Boolean(LoggedIn)
+console.log(boolLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Yash" => true
+
+let num = 33
+let strnum = String(num);
+
+console.log(strnum);
+console.log(typeof strnum);
+
