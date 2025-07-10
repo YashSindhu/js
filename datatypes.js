e8844110d479893378a446796b3868729f8d6235
Yash Sindhu <yashsindhu123@gmail.com>
2025-07-10 15:17:18 +0530

    Datatypes

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // Treat all JS code as of newer version.
+//  alert(3+3) // We are using NodJS not browser so diff way,
+// Sometimes we use semicolans but sometimes not
+// console.log("Yash") console.log("Sindhu") Here it will give error but
+// console.log("Yash"); console.log("Sindhu") => this is okay
+console.log("Yash")  // This is also okay
+console.log("Sindhu")
+
+let name = "Yash"
+let age = 21
+let IsLoggedIn = false
+let state
+
+console.log(typeof age); // => number
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undifined
