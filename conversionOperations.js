6bd37fa164aa83357972685e857724e2e2fdf0a2
Yash Sindhu <yashsindhu123@gmail.com>
2025-07-11 11:11:58 +0530

    ComparisionAndConversionOperations

--- conversionOperations.js
+++ conversionOperations.js
@@ -10,7 +10,7 @@ let valueInNumber = Number(score)
 
 let LoggedIn = "Yash"
 let boolLoggedIn = Boolean(LoggedIn)
-console.log(boolLoggedIn);
+// console.log(boolLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -19,6 +19,45 @@ console.log(boolLoggedIn);
 let num = 33
 let strnum = String(num);
 
-console.log(strnum);
-console.log(typeof strnum);
+// console.log(strnum);
+// console.log(typeof strnum);
+
+
+// ************************ Operations ************************
+
+let val = 3
+let negValue = -val
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Yash"
+let str2 = " Sindhu"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2); // => 12
+// console.log(1+"2"); // => 12
+// console.log("1"+2+2); // => 122
+// console.log(1+2+"2"); // => 32
+
+// console.log(true); // After writing plus data conversion occurs
+// console.log(+true); // console.log(true+) => will give error
+// console.log(-true);
+// console.log(+""); // => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
 
